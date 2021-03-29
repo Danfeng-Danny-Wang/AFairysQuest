f3cfdef040f343e150c3f8df68cf2561b0ad755f
Danfeng Wang <dannywang@utexas.edu>
2021-03-29 15:54:39 -0400

    basic setup for level 3

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO);
 game.state.add("level0", levels.level0);
 game.state.add("level1", levels.level1);
 game.state.add("level2", levels.level2);
+game.state.add("level3", levels.level3);
 game.state.add("result", levels.result);
 
 game.playerStats = {
@@ -13,4 +14,4 @@ game.playerStats = {
     startingPosY: 15,
 };
 
-game.state.start("level0");
+game.state.start("level3");
